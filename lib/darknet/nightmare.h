ae8fd3b6713743c39a988421de741a6c64364560
Marcel Schwittlick <marzzzel@gmail.com>
2016-12-14 23:31:38 +0100

    adds some header files

---
+++ lib/darknet/nightmare.h
@@ -0,0 +1,19 @@
+#ifndef NIGHTMARE_H
+#define NIGHTMARE_H
+
+
+	float abs_mean( float *x, int n );
+	void calculate_loss( float *output, float *delta, int n, float thresh );
+#ifdef __cplusplus 
+	extern "C" {
+#endif
+		void optimize_picture( network *net, image orig, int max_layer, float scale, float rate, float thresh, int norm );
+		void reconstruct_picture( network net, float *features, image recon, image update, float rate, float momentum, float lambda, int smooth_size, int iters );
+#ifdef __cplusplus 
+	}
+#endif
+	void smooth( image recon, image update, float lambda, int num );
+	
+
+
+#endif
