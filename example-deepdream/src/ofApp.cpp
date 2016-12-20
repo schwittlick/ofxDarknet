ed613cefa83515d13f86abad0d276a79d6ccada7
Marcel Schwittlick <marzzzel@gmail.com>
2016-12-20 21:59:52 +0100

    updates deepdream example

--- example-deepdream/src/ofApp.cpp
+++ example-deepdream/src/ofApp.cpp
@@ -8,7 +8,16 @@ void ofApp::setup()
 	std::string cfgfile = "cfg/vgg-conv.cfg";
 	std::string weightfile = "vgg-conv.weights";
 	darknet.init( cfgfile, weightfile );
-	nightmare = darknet.nightmate( dog.getPixelsRef() );
+	
+	int max_layer = 13;
+	int range = 3;
+	int norm = 1;
+	int rounds = 4;
+	int iters = 20;
+	int octaves = 4;
+	float rate = 0.01;
+	float thresh = 1.0;
+	nightmare = darknet.nightmate( dog.getPixelsRef(), max_layer, range, norm, rounds, iters, octaves, rate, thresh );
 }
 
 void ofApp::update()
