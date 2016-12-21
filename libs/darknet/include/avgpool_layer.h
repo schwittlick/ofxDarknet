0d62f5aaeb1dbc430252ebf0eccdc9123d4d562e
Marcel Schwittlick <marzzzel@gmail.com>
2016-12-21 12:37:32 +0100

    moves the darknet c source code to appropriate location for project generator

--- libs/darknet/include/avgpool_layer.h
+++ libs/darknet/include/avgpool_layer.h
@@ -8,6 +8,8 @@
 
 typedef layer avgpool_layer;
 
+
+
 image get_avgpool_image(avgpool_layer l);
 avgpool_layer make_avgpool_layer(int batch, int w, int h, int c);
 void resize_avgpool_layer(avgpool_layer *l, int w, int h);
