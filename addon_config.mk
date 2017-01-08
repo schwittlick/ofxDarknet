590de2778bf7a882a7870580b2a3011cd808839b
Gene Kogan <kogan.gene@gmail.com>
2017-01-08 17:37:20 -0500

    manually including headers in addons makefile

--- addon_config.mk
+++ addon_config.mk
@@ -29,7 +29,9 @@ common:
 	# include search paths, this will be usually parsed from the file system
 	# but if the addon or addon libraries need special search paths they can be
 	# specified here separated by spaces or one per line using +=
-	# ADDON_INCLUDES =
+	ADDON_INCLUDES = libs
+	ADDON_INCLUDES += src
+	ADDON_INCLUDES += /usr/local/cuda/include
 	
 	# any special flag that should be passed to the compiler when using this
 	# addon
@@ -50,8 +52,57 @@ common:
 	# in the src folders in libs and the root of the addon. if your addon needs
 	# to include files in different places or a different set of files per platform
 	# they can be specified here
-	# ADDON_SOURCES =
-	
+	ADDON_SOURCES = src/ofxDarknet.cpp
+	ADDON_SOURCES += src/ofxDarknet.h
+	ADDON_SOURCES += libs/darknet/include/activation_layer.h
+	ADDON_SOURCES += libs/darknet/include/activations.h
+	ADDON_SOURCES += libs/darknet/include/avgpool_layer.h
+	ADDON_SOURCES += libs/darknet/include/batchnorm_layer.h
+	ADDON_SOURCES += libs/darknet/include/blas.h
+	ADDON_SOURCES += libs/darknet/include/box.h
+	ADDON_SOURCES += libs/darknet/include/classifier.h
+	ADDON_SOURCES += libs/darknet/include/col2im.h
+	ADDON_SOURCES += libs/darknet/include/connected_layer.h
+	ADDON_SOURCES += libs/darknet/include/convolutional_layer.h
+	ADDON_SOURCES += libs/darknet/include/cost_layer.h
+	ADDON_SOURCES += libs/darknet/include/crnn_layer.h
+	ADDON_SOURCES += libs/darknet/include/crop_layer.h
+	ADDON_SOURCES += libs/darknet/include/cuda.h
+	ADDON_SOURCES += libs/darknet/include/data.h
+	ADDON_SOURCES += libs/darknet/include/deconvolutional_layer.h
+	ADDON_SOURCES += libs/darknet/include/demo.h
+	ADDON_SOURCES += libs/darknet/include/detection_layer.h
+	ADDON_SOURCES += libs/darknet/include/dropout_layer.h
+	ADDON_SOURCES += libs/darknet/include/gemm.h
+	ADDON_SOURCES += libs/darknet/include/getopt.h
+	ADDON_SOURCES += libs/darknet/include/gettimeofday.h
+	ADDON_SOURCES += libs/darknet/include/gru_layer.h
+	ADDON_SOURCES += libs/darknet/include/im2col.h
+	ADDON_SOURCES += libs/darknet/include/image.h
+	ADDON_SOURCES += libs/darknet/include/layer.h
+	ADDON_SOURCES += libs/darknet/include/list.h
+	ADDON_SOURCES += libs/darknet/include/local_layer.h
+	ADDON_SOURCES += libs/darknet/include/matrix.h
+	ADDON_SOURCES += libs/darknet/include/maxpool_layer.h
+	ADDON_SOURCES += libs/darknet/include/network.h
+	ADDON_SOURCES += libs/darknet/include/nightmare.h
+	ADDON_SOURCES += libs/darknet/include/normalization_layer.h
+	ADDON_SOURCES += libs/darknet/include/option_list.h
+	ADDON_SOURCES += libs/darknet/include/parser.h
+	ADDON_SOURCES += libs/darknet/include/region_layer.h
+	ADDON_SOURCES += libs/darknet/include/reorg_layer.h
+	ADDON_SOURCES += libs/darknet/include/rnn_layer.h
+	ADDON_SOURCES += libs/darknet/include/rnn_vid.h
+	ADDON_SOURCES += libs/darknet/include/rnn.h
+	ADDON_SOURCES += libs/darknet/include/route_layer.h
+	ADDON_SOURCES += libs/darknet/include/shortcut_layer.h
+	ADDON_SOURCES += libs/darknet/include/softmax_layer.h
+	ADDON_SOURCES += libs/darknet/include/stb_image_write.h
+	ADDON_SOURCES += libs/darknet/include/stb_image.h
+	ADDON_SOURCES += libs/darknet/include/tree.h
+	ADDON_SOURCES += libs/darknet/include/unistd.h
+	ADDON_SOURCES += libs/darknet/include/utils.h
+
 	# some addons need resources to be copied to the bin/data folder of the project
 	# specify here any files that need to be copied, you can use wildcards like * and ?
 	# ADDON_DATA = 
@@ -59,6 +110,8 @@ common:
 	# when parsing the file system looking for libraries exclude this for all or
 	# a specific platform
 	# ADDON_LIBS_EXCLUDE =
+
+	ADDON_LIBS_INCLUDE = /usr/helloworld
 	
 msys2:
 
