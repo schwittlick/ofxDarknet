22736989844d1c6866aae8af0628376e92df7f73
Gene Kogan <kogan.gene@gmail.com>
2017-05-07 12:32:49 +0100

    deepdream example uses webcam

--- example-deepdream/src/ofApp.cpp
+++ example-deepdream/src/ofApp.cpp
@@ -1,30 +1,69 @@
 #include "ofApp.h"
 
-void ofApp::setup() 
+void ofApp::setup()
 {
-	dog.loadImage( "dog.jpg" );
-	dog.resize( 640, 480 );
-	
-	std::string cfgfile = ofToDataPath( "cfg/vgg-conv.cfg" );
-	std::string weightfile = ofToDataPath( "vgg-conv.weights" );
-	darknet.init( cfgfile, weightfile );
-	
-	int max_layer = 13;
-	int range = 3;
-	int norm = 1;
-	int rounds = 4;
-	int iters = 20;
-	int octaves = 4;
-	float rate = 0.01;
-	float thresh = 1.0;
-	nightmare = darknet.nightmare( dog.getPixels(), max_layer, range, norm, rounds, iters, octaves, rate, thresh );
+    string cfgfile = "cfg/vgg-conv.cfg";
+    string weightfile = "vgg-conv.weights";
+    darknet.init( cfgfile, weightfile );
+
+    isDreaming.addListener(this, &ofApp::startDream);
+    
+    gui.setup();
+    gui.setName("darkdream");
+    gui.add(isDreaming.set("dream", false));
+    gui.add(max_layer.set("max_layer", 13, 1, 13));
+    gui.add(iters.set("iterations", 1, 1, 10));
+    gui.add(octaves.set("octaves", 2, 1, 8));
+    gui.add(thresh.set("thresh", 0.85, 0.0, 1.0));
+    gui.add(range.set("range", 3, 1, 10));
+    gui.add(norm.set("norm", 1, 1, 10));
+    gui.add(rate.set("rate", 0.01, 0.0, 0.1));
+    gui.add(blendAmt.set("blendAmt", 0.5, 0.0, 1.0));
+
+    cam.initGrabber(320, 240);
+    
+}
+
+void ofApp::makeDream()
+{
+    // blend last dream with cam
+    ofPixels p1 = nightmare.getPixels();
+    ofPixels p2 = cam.getPixels();
+    ofPixels pix;
+    pix.allocate(p1.getWidth(), p2.getHeight(), 3);
+    for (int i=0; i<pix.size(); i++) {
+        pix[i] = blendAmt * p1[i] + (1.0 - blendAmt) * p2[i];
+    }
+    
+    // dream
+    nightmare = darknet.nightmare( pix, max_layer, range, norm, 1, iters, octaves, rate, thresh );
 }
 
 void ofApp::update()
 {
+    cam.update();
+    
+    if (isDreaming) {
+        makeDream();
+    }
 }
 
 void ofApp::draw()
 {
-	nightmare.draw( 0, 0, ofGetWidth(), ofGetHeight() );
+    cam.draw(300, 0);
+    if (nightmare.isAllocated()){
+        nightmare.draw(300, 260);
+    }
+    gui.draw();
+}
+
+void ofApp::startDream(bool & b) {
+    if (b) {
+        nightmare.setFromPixels(cam.getPixels());
+        makeDream();
+    }
+}
+
+void ofApp::keyPressed(int key) {
+    
 }
--- example-deepdream/src/ofApp.h
+++ example-deepdream/src/ofApp.h
@@ -3,6 +3,7 @@
 #include "ofMain.h"
 
 #include "ofxDarknet.h"
+#include "ofxGui.h"
 
 class ofApp : public ofBaseApp
 {
@@ -10,8 +11,25 @@ public:
 	void setup();
 	void update();
 	void draw();
-
+    void keyPressed(int key);
+    
+    void makeDream();
+    void startDream(bool & b);
+    
 	ofxDarknet darknet;
-	ofImage dog, nightmare;
+    ofVideoGrabber cam;
+	ofImage nightmare;
+    
+    ofParameter<bool> isDreaming;
+    ofParameter<int> max_layer;
+    ofParameter<int> iters;
+    ofParameter<int> octaves;
+    ofParameter<float> thresh;
+    ofParameter<int> range;
+    ofParameter<int> norm;
+    ofParameter<float> rate;
+    ofParameter<float> blendAmt;
+
+    ofxPanel gui;
 };
 
